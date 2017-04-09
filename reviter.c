2ab71163e1c5fa4e59eb7d4d7744e632ad8d5cb5
hoggetaylor <hoggetaylor@gmail.com>
2017-04-09 11:47:42 -0600

    fix segault in reviter.c

--- reviter.c
+++ reviter.c
@@ -74,9 +74,9 @@ int main() {
     }
 
     for (n = 0; n < nmax; n++) {
-        for (i = 1; i < imax; i++) {
+        for (i = 0; i < imax; i++) {
             for (j = 1; j < jmax; j++) {
-                for (k = 0; k < kmax; k++) {
+                for (k = 1; k < kmax; k++) {
                     Ex[i][j][k] = Ca*Ex[i][j][k] + Cb*((Hz[i][j][k] - Hy[i][j-1][k]) + (Hy[i][j][k-1] - Hy[i][j][k]));
                 }
             }
@@ -88,9 +88,9 @@ int main() {
                 }
             }
         }
-        for (i = 0; i < imax; i++) {
+        for (i = 1; i < imax; i++) {
             for (j = 1; j < jmax; j++) {
-                for (k = 1; k < kmax; k++) {
+                for (k = 0; k < kmax; k++) {
                     Ez[i][j][k] = Ca*Ez[i][j][k] + Cb*((Hz[i][j][k] - Hy[i-1][j][k]) + (Hy[i][j-1][k] - Hy[i][j][k]));
                     if(n==200) {
                         memset(buf, 0, 18);
@@ -101,9 +101,9 @@ int main() {
             }
         }
         Ez[imax/2][jmax/2][kmax/2] = exp(-(pow(((n-no)/(double)nhalf),2.0)));
-        for (i = 0; i < imax; i++) {
+        for (i = 1; i < imax; i++) {
            for (j = 0; j < jmax-1; j++) {
-               for (k = 1; k < kmax-1; k++) {
+               for (k = 0; k < kmax-1; k++) {
                    Hx[i][j][k] = Da*Hx[i][j][k] + Db*((Ez[i][j][k] - Ez[i][j+1][k]) + (Ez[i][j][k+1]-Ez[i][j][k]));
                }
            }
@@ -115,9 +115,9 @@ int main() {
                }
            }
         }
-        for (i = 1; i < imax; i++) {
+        for (i = 0; i < imax; i++) {
            for (j = 0; j < jmax-1; j++) {
-               for (k = 0; k < kmax-1; k++) {
+               for (k = 1; k < kmax-1; k++) {
                    Hz[i][j][k] = Da*Hz[i][j][k] + Db*((Ez[i][j][k] - Ez[i+1][j][k]) + (Ez[i][j+1][k]-Ez[i][j][k]));
                }
            }
