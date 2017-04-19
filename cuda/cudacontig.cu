9ca2f2ec8e49bb72a975c237047bf1af05929656
hoggetaylor <hoggetaylor@gmail.com>
2017-04-19 16:06:19 -0600

    Use new array syntax

--- cuda/cudacontig.cu
+++ cuda/cudacontig.cu
@@ -7,6 +7,11 @@
 #include <sys/time.h>
 #include <cuda_runtime.h>
 
+#define IMAX 100;
+#define JMAX 100;
+#define KMAX 100;
+
+
 // This was taken from stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
 #define CHECK_ERROR(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line) {
@@ -19,26 +24,30 @@ inline void gpuAssert(cudaError_t code, const char *file, int line) {
 /**
  *  Second half of the total loop circuit.
  */
-extern __global__ void loop2_GPU(double* Hx, double* Ez, double Da, double Db, int kmax, int jmax, int imax) {
+extern __global__ void loop2_GPU(
+        double (*Ez)[IMAX][JMAX], 
+        double (*Hx)[IMAX][JMAX], 
+        double (*Hy)[IMAX][JMAX], 
+        double (*Hz)[IMAX][JMAX],
+        double Da, double Db) {
     int i, j;
     int k = blockIdx.x * 32 + threadIdx.x;
-    if (k < kmax) {
-        for (j = 0; j < jmax-1; j++) {
-            for (i = 1; i < imax-1; i++) {
+
+    if (k < KMAX && k > 0) {
+        for (j = 0; j < JMAX-1; j++) {
+            for (i = 1; i < IMAX-1; i++) {
                 Hx[i][j][k] = Da*Hx[i][j][k] + Db*((Ez[i][j][k] - Ez[i][j+1][k]) + (Ez[i][j][k+1]-Ez[i][j][k]));
             }
         }
-    }
-    if (k < kmax) {
-       for (j = 1; j < jmax-1; j++) {
-           for (i = 0; i < imax-1; i++) {
+       for (j = 1; j < JMAX-1; j++) {
+           for (i = 0; i < IMAX-1; i++) {
                Hy[i][j][k] = Da*Hy[i][j][k] + Db*((Ez[i+1][j][k] - Ez[i][j][k]) + (Ez[i][j][k]-Ez[i][j][k+1]));
            }
        }
     }
-    if (k < kmax) {
-       for (j = 0; j < jmax-1; j++) {
-           for (i = 0; i < imax-1; i++) {
+    if (k < KMAX) {
+       for (j = 0; j < JMAX-1; j++) {
+           for (i = 0; i < IMAX-1; i++) {
                Hz[i][j][k] = Da*Hz[i][j][k] + Db*((Ez[i][j][k] - Ez[i+1][j][k]) + (Ez[i][j+1][k]-Ez[i][j][k]));
            }
        }
@@ -46,7 +55,7 @@ extern __global__ void loop2_GPU(double* Hx, double* Ez, double Da, double Db, i
 }
 
 int main() {
-    int imax = 100, jmax = 100, nmax = 1000, nhalf = 20, no = nhalf*3, kmax = 100;
+    int nmax = 1000, nhalf = 20, no = nhalf*3;
     int i, j, n,k;
     double c = 2.99792458e8, pi = 3.141592654, sigma = 0, mu = 4.0 * pi * 1.0e-7, eps = 8.85418782e-12;
     double delta = 1e-3;
@@ -96,32 +105,32 @@ int main() {
     CHECK_ERROR(cudaMemcpy(g_Hz, Hz, (imax+1) * (jmax+1) * (kmax+1) * sizeof(double), cudaMemcpyHostToDevice));
 
     for (n = 0; n < nmax; n++) {
-        for (k = 1; k < kmax; k++) {
-            for (j = 1; j < jmax; j++) {
-                for (i = 0; i < imax; i++) {
-                    Ex[i][j][k] = Ca*Ex[i][j][k] + Cb*((Hz[i][j][k] - Hy[i][j-1][k]) + (Hy[i][j][k-1] - Hy[i][j][k]));
-                }
-            }
-        }
-        for (k = 1; k < kmax; k++) {
-            for (j = 0; j < jmax; j++) {
-                for (i = 1; i < imax; i++) {
-                    Ey[i][j][k] = Ca*Ey[i][j][k] + Cb*((Hz[i-1][j][k] - Hy[i][j][k]) + (Hy[i][j][k] - Hy[i][j][k-1]));
-                }
-            }
-        }
-        for (k = 0; k < kmax; k++) {
-            for (j = 1; j < jmax; j++) {
-                for (i = 1; i < imax; i++) {
-                    Ez[i][j][k] = Ca*Ez[i][j][k] + Cb*((Hz[i][j][k] - Hy[i-1][j][k]) + (Hy[i][j-1][k] - Hy[i][j][k]));
-                }
-            }
-        }
-        Ez[imax/2][jmax/2][kmax/2] = exp(-(pow(((n-no)/(double)nhalf),2.0)));
+        //for (k = 1; k < kmax; k++) {
+        //    for (j = 1; j < jmax; j++) {
+        //        for (i = 0; i < imax; i++) {
+        //            Ex[i][j][k] = Ca*Ex[i][j][k] + Cb*((Hz[i][j][k] - Hy[i][j-1][k]) + (Hy[i][j][k-1] - Hy[i][j][k]));
+        //        }
+        //    }
+        //}
+        //for (k = 1; k < kmax; k++) {
+        //    for (j = 0; j < jmax; j++) {
+        //        for (i = 1; i < imax; i++) {
+        //            Ey[i][j][k] = Ca*Ey[i][j][k] + Cb*((Hz[i-1][j][k] - Hy[i][j][k]) + (Hy[i][j][k] - Hy[i][j][k-1]));
+        //        }
+        //    }
+        //}
+        //for (k = 0; k < kmax; k++) {
+        //    for (j = 1; j < jmax; j++) {
+        //        for (i = 1; i < imax; i++) {
+        //            Ez[i][j][k] = Ca*Ez[i][j][k] + Cb*((Hz[i][j][k] - Hy[i-1][j][k]) + (Hy[i][j-1][k] - Hy[i][j][k]));
+        //        }
+        //    }
+        //}
+        //Ez[imax/2][jmax/2][kmax/2] = exp(-(pow(((n-no)/(double)nhalf),2.0)));
 
         dim3 threadsPerBlock(32);
         dim3 numBlocks((kmax + threadsPerBlock.x-1) / threadsPerBlock.x);
-        loop2_GPU<<<numBlocks, threadsPerBlock>>>(g_Hx, g_Ez, Da, Db, kmax, jmax, imax);
+        loop2_GPU<<<numBlocks, threadsPerBlock>>>(g_Ez, g_Hx, g_Hy, g_Hz, Da, Db);
     }
 
     CHECK_ERROR(cudaMemcpy(Ex, g_Ex, (imax+1) * (jmax+1) * (kmax+1) * sizeof(double), cudaMemcpyDeviceToHost));
